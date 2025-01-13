4ce95a1fadb3dc36770bab1bd51e20f20f329bbc
aditya-3761 <aditya6112005@gmail.com>
2025-01-13 12:08:54 +0530

    done

--- javascript/maths.js
+++ javascript/maths.js
@@ -6,3 +6,18 @@ document.write("FLOOR VALUE = ",Math.floor(4.6))
 document.write("TRUNCATION = ",Math.trunc(4.6))
 document.write("ABSOLUTE = ",Math.abs(4.6))
 
+
+Math.floor(Math.random()*10);
+Math.floor(Math.random()*11);
+Math.floor(Math.random()*100);
+Math.floor(Math.random()*101);
+Math.floor(Math.random()*10)+1;
+
+function getRndInteger(min,max){
+    return Math.floor(Math.random()*(max-min))+min;
+}
+
+Number("3.14")
+Number(Math.PI)
+Number("99 88")
+Number("John")
